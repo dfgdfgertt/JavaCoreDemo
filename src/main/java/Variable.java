07fa6210ff956a8ec89ef4aad3c907c66fec2caa
dfgdfgertt <majinsyn@gmail.com>
2021-08-04 09:53:09 +0700

    update day 1

---
+++ src/main/java/Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args) {
+        var str = "Thế giới của Long!!!";
+        System.out.println(str);
+
+        Integer a = 100,b= 200;
+        swap(a,b);
+        System.out.println("a= "+ a +" b= "+b );
+
+    }
+
+    public static void swap(Integer a, Integer b){
+        var temp = a;
+        a=b;
+        b=temp;
+    }
+}
